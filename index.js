0019121ae38fd92241790e85e9fcdc51809547ab
Nicola Antonio Filannino <filanico@gmail.com>
2022-10-08 21:52:42 +0000

    Added portRange brute-force listening

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const fs = require('fs')
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3001;
+const portRange = [80,8080];
 
 //const serverless = require('serverless-http');
 app.use(cors())
-app.listen(port, () => console.log("Server started !"))
+let portRangeIndex=0;
+do{  
+  let failed=false
+  try{
+    app.listen(portRange[i++], () => console.log("Server started !"))
+  } catch(failure){
+    console.log("ERROR - failed using port "+portRange[i-1]);
+    failed=true;
+  }
+} while(failed);
 
 app.get('/', function(req, res) {
   const search = req.query.search.toUpperCase();
